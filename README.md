603cbbb94bf280e8cff91dddb441a298be532f29
Vladimir Lazić <contact@vlazic.com>
2021-11-02 03:00:50 +0100

    feat: Added self-installation

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # Screen Selfie
+
 Bash script that takes screenshoots in regular intervals
 
+## Installation
+
+```bash
+wget https://raw.githubusercontent.com/vlazic/screen-selfie/master/screen-selfie.sh
+chmod +x screen-selfie.sh
+./screen-selfie.sh
+# check if everything works with:
+sh -c 'XAUTHORITY=~/.Xauthority DISPLAY=:0 ~/screen-selfie.sh'
+ls ~/screen-selfie/*
+```
+
 ## TODO
-- add installation file
+
 - add REMOVE_FILES_SMALLER_THEN variable
--- screen-selfie.sh
+++ screen-selfie.sh
@@ -1,38 +1,36 @@
-#!/bin/bash
-
-# place this file in your home directory:
-# wget -P ~ https://raw.githubusercontent.com/vlazic/screen-selfie/master/screen-selfie.sh
-# chmod +x ~/screen-selfie.sh
-
-# install dependencies with: 
-# sudo apt install -y scrot 
-
-# create ~/.Xauthority link
-# ln -s -f "$XAUTHORITY" ~/.Xauthority
-
-# check if everything works with:
-# sh -c 'XAUTHORITY=~/.Xauthority DISPLAY=:0 ~/screen-selfie.sh'
-# ls ~/screen-selfie/*
-
-# add to your crontab file:
-# */5 * * * * XAUTHORITY=~/.Xauthority DISPLAY=:0 ~/screen-selfie.sh >> ~/screen-selfie.log 2>&1
-# if DISPLAY=:0 doesn't work, get your display number with: 
-# ps e | sed -rn 's/.* DISPLAY=(:[0-9]*).*/\1/p' | head -1
-
-# you can overwrite default configuration variables by prefixing Cron command
-# available configuration variables: SCREENSHOTS_DIR, JPEG_QUALITY, FILE_TEMPLATE
-
-# TODO: create installation script
+#!/usr/bin/env bash
 
 DEFAULT_JPEG_QUALITY=25
-DEFAULT_SCREENSHOTS_DIR=~/screen-selfie
+DEFAULT_SCREENSHOTS_DIR="${HOME}/screen-selfie"
 DEFAULT_FILE_TEMPLATE="%Y-%m-%d_%H-%M-%S.jpeg"
+CRON_FILE="/etc/cron.d/screen-selfie"
 
-# if values of following variables are not passed to the 
+# if values of following variables are not passed to the
 # script use default ones
 SCREENSHOTS_DIR="${SCREENSHOTS_DIR:-$DEFAULT_SCREENSHOTS_DIR}"
 JPEG_QUALITY="${JPEG_QUALITY:-$DEFAULT_JPEG_QUALITY}"
 FILE_TEMPLATE="${FILE_TEMPLATE:-$DEFAULT_FILE_TEMPLATE}"
+
+command -v scrot >/dev/null 2>&1 || {
+	sudo apt install -y scrot
+}
+
+# if ~/.Xauthority file is missing create it
+if [ ! -f ~/.Xauthority ]; then
+	echo "Creating ~/.Xauthority file"
+	ln -s -f "$XAUTHORITY" "${HOME}/.Xauthority"
+fi
+
+if [ ! -f $CRON_FILE ]; then
+	echo "Creating cron file"
+
+	# you can overwrite default configuration variables by prefixing Cron command
+	# available configuration variables: SCREENSHOTS_DIR, JPEG_QUALITY, FILE_TEMPLATE
+	echo "*/5 * * * * ${USER} XAUTHORITY=${HOME}/.Xauthority DISPLAY=$(ps e | sed -rn 's/.* DISPLAY=(:[0-9]*).*/\1/p' | head -1)  ${0} | logger -t screen-selfie 2>&1" |
+		sudo tee $CRON_FILE && echo "Cron file created"
+
+	sudo chmod +x $CRON_FILE
+fi
 
 # directory where image will be placed
 # every day new folder will be created
@@ -42,9 +40,9 @@ SCREENSHOTS_TODAY_DIR="${SCREENSHOTS_DIR}/$(date +%F)"
 mkdir -p "$SCREENSHOTS_TODAY_DIR"
 
 # take screen-shoot
-scrot	-m -z \
-		-q "$JPEG_QUALITY" \
-		-b "${SCREENSHOTS_TODAY_DIR}/${FILE_TEMPLATE}"
+scrot -m -z \
+	-q "$JPEG_QUALITY" \
+	-b "${SCREENSHOTS_TODAY_DIR}/${FILE_TEMPLATE}"
 
 # if there is a lot of black images you can delete with this line
 # (replace image size to match your black image size)
